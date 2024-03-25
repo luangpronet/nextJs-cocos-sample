eaac7538d1bcf08741f37de458ad628fd13ba64b
luan.votrong.gpronet <49549751+luangpronet@users.noreply.github.com>
2024-03-25 17:14:48 +0700

    Remove redundant case file

--- app/import/components/cocos.tsx
+++ app/import/components/cocos.tsx
@@ -4,22 +4,25 @@ import Script from "next/script";
 import styles from "./cocos.module.css";
 
 interface CocosProps {
-    isShow : boolean
+  isShow: boolean;
 }
 
-export default function Cocos(props : CocosProps) {
-    return (
-        <div className={styles.game_div}>
-        <canvas id="GameCanvas" className={styles.game_canvas}></canvas>
-            <Script src="./src/system.bundle.js"></Script>
+export default function Cocos(props: CocosProps) {
+  const CanVasStyles = props.isShow
+    ? styles.game_div
+    : `${styles.game_div} ${styles.inactive}`;
 
-            <Script
-                src="./src/import-map.json"
-                type="systemjs-importmap"
-            ></Script>
-            <Script
-                src="cocos_loader.js"
-            ></Script>
-            </div>
-    );
+  return (
+    <div id="nthiendev" className={CanVasStyles}>
+      {/* <canvas id="GameCanvas" className={styles.game_canvas}></canvas> */}
+      {/* <Script id="cocos-nthiendev" src="./src/system.bundle.js"></Script>
+
+      <Script
+        id="cocos-nthiendev"
+        src="./src/import-map.json"
+        type="systemjs-importmap"
+      ></Script>
+      <Script id="cocos-nthiendev" src="cocos_loader.js"></Script> */}
+    </div>
+  );
 }
